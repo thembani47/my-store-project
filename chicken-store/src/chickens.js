9dfda0ebea0a066dbdbcb50af65bb06a85646b59
Thembani Maswanganyi <t.maswanganyi101@gmail.com>
2023-08-17 15:55:59 +0200

    create project

---
+++ chicken-store/src/chickens.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+      "name": "Rooster",
+      "price": 250.50,
+      "image": "/assests/img/rooster.jpg",
+      "description": "Rooster and cock are terms for adult male birds, and a younger male may be called a cockerel. A male that has been castrated is a capon"
+    },
+    {
+        "name": "Egg Laying",
+        "price": 200,
+        "image": "/assests/img/egg-laying.jpg",
+        "description": "Rooster and cock are terms for adult male birds, and a younger male may be called a cockerel. A male that has been castrated is a capon"
+      }
+    
+  ]
